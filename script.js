fc2373bf48ff804efd1ecd8bde05b18191d0692a
softacoder <113185323+softacoder@users.noreply.github.com>
2023-08-25 13:16:03 +0100

    commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 async function fetchData() {
-  const url =
-    "https://concerts-artists-events-tracker.p.rapidapi.com/location?name=London&minDate=2023-08-25&maxDate=2023-08-31&page=1";
+  // const url = 'https://concerts-artists-events-tracker.p.rapidapi.com/venue/past?name=Hollywood%20bowl&minDate=2022-01-25&maxDate=2023-01-30&page=1';
   const options = {
     method: "GET",
     headers: {
@@ -9,11 +8,20 @@ async function fetchData() {
     },
   };
 
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
+  const res = await fetch(
+    "https://concerts-artists-events-tracker.p.rapidapi.com/venue/past?name=Hollywood%20bowl&minDate=2022-01-25&maxDate=2023-01-30&page=1"
+  );
+  const record = await res.json();
+
+  document.getElementById("concerts").innerHTML = record.data
+    .map((item) => `<li>${item.name}</li>`)
+    .join("");
+  // try {
+  // 	const response = await fetch(url, options);
+  // 	const result = await response.text();
+  // 	console.log(result);
+  // } catch (error) {
+  // 	console.error(error);
+  // }
 }
+fetchData();
